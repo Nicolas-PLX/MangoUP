255d0eec457db49940328b78fb756838659f986a
peneloux <nicolas.peneloux@free.fr>
2025-02-08 21:00:26 +0100

    load / save working, improve GUI

--- src/Launcher/model.py
+++ src/Launcher/model.py
@@ -1,8 +1,13 @@
-
+import Save.configjson as cj
 
 class Model:
+    list_title = []
 
     def __init__(self):
-        pass
-
-    
+        self.saves_path = "../saves/save.json"
+        
+    def load(self):
+        self.list_title = cj.load_json(self.saves_path)
+        
+    def save(self):
+        cj.save_json(self.saves_path,self.list_title)
